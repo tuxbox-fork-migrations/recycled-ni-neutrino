tuxbox-fork-migrations/recycled-ni-neutrino

commit 0f093fe680d6d74f3bab3d5e856d2eb215db0ada

videosettings.cpp: hide zapmode settings for boxes that dont have it (e.g. osmio)
Origin commit data
------------------
Branch: master
Commit: https://github.com/neutrino-images/ni-neutrino/commit/930366584e9c161cd6ef1a9b3d235b91b60677ff
Author: Markus Volk <f_l_k@t-online.de>
Date: 2020-03-05 (Thu, 05 Mar 2020)


------------------
No further description and justification available within origin commit message!

------------------
This commit was generated by Migit

src/gui/videosettings.cpp
@@ -55,6 +55,7 @@
 #include <daemonc/remotecontrol.h>
 
 #include <system/debug.h>
+#include <system/helpers.cpp>
 
 #include <cs_api.h>
 #include <hardware/video.h>
@@ -488,12 +489,11 @@ int CVideoSettings::showVideoSetup()
 	videosetup->addItem(pipsetup);
 #endif
 
-#if HAVE_ARM_HARDWARE || HAVE_MIPS_HARDWARE
-	CMenuOptionChooser * zm = new CMenuOptionChooser(LOCALE_VIDEOMENU_ZAPPINGMODE, &g_settings.zappingmode, VIDEOMENU_ZAPPINGMODE_OPTIONS, VIDEOMENU_ZAPPINGMODE_OPTION_COUNT, true, this);
-	zm->setHint("", LOCALE_MENU_HINT_VIDEO_ZAPPINGMODE);
-	videosetup->addItem(zm);
-#endif
-
+	if (file_exists("/proc/stb/video/zapmode")) {
+		CMenuOptionChooser * zm = new CMenuOptionChooser(LOCALE_VIDEOMENU_ZAPPINGMODE, &g_settings.zappingmode, VIDEOMENU_ZAPPINGMODE_OPTIONS, VIDEOMENU_ZAPPINGMODE_OPTION_COUNT, true, this);
+		zm->setHint("", LOCALE_MENU_HINT_VIDEO_ZAPPINGMODE);
+		videosetup->addItem(zm);
+	}
 	int res = videosetup->exec(NULL, "");
 	selected = videosetup->getSelected();
 	delete videosetup;

commit f805eae8f78208c5170419f44c1096f60d13396b

fix build; broken since 0f093fe
Origin commit data
------------------
Branch: master
Commit: https://github.com/neutrino-images/ni-neutrino/commit/37bf3ee0e03e59a43295cee96454e22ea22c960c
Author: vanhofen <vanhofen@gmx.de>
Date: 2020-03-06 (Fri, 06 Mar 2020)

Origin message was:
------------------
- fix build; broken since 0f093fe

------------------
No further description and justification available within origin commit message!

------------------
This commit was generated by Migit

src/gui/videosettings.cpp
@@ -328,14 +328,12 @@ const CMenuOptionChooser::keyval VIDEOMENU_DBDR_OPTIONS[VIDEOMENU_DBDR_OPTION_CO
 	{ 2, LOCALE_VIDEOMENU_DBDR_BOTH }
 };
 
-#if HAVE_ARM_HARDWARE || HAVE_MIPS_HARDWARE
 #define VIDEOMENU_ZAPPINGMODE_OPTION_COUNT 2
 CMenuOptionChooser::keyval VIDEOMENU_ZAPPINGMODE_OPTIONS[VIDEOMENU_ZAPPINGMODE_OPTION_COUNT] =
 {
 	{ 0, LOCALE_VIDEOMENU_ZAPPINGMODE_MUTE },
 	{ 1, LOCALE_VIDEOMENU_ZAPPINGMODE_HOLD }
 };
-#endif
 
 int CVideoSettings::showVideoSetup()
 {

src/system/settings.h
@@ -222,9 +222,7 @@ struct SNeutrinoSettings
 	int record_safety_time_before;
 	int record_safety_time_after;
 	int zapto_pre_time;
-#if HAVE_ARM_HARDWARE || HAVE_MIPS_HARDWARE
 	int zappingmode;
-#endif
 	int infobar_sat_display;
 	int infobar_show_channeldesc;
 	int infobar_subchan_disp_pos;